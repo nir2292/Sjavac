f8476b1ec057c4afb7938a6d02db9a4f568a63bf
dor132 <da19965@gmail.com>
2015-06-12 17:44:45 +0300

    Fix

--- src/oop/ex6/main/Parser.java
+++ src/oop/ex6/main/Parser.java
@@ -10,10 +10,11 @@ import java.util.regex.Pattern;
 import oop.ex6.scopes.*;
 
 public class Parser {
-	final String varLineRegex = "(int|double|String|char|boolean)\\w+\\s+\\=\\w+";
+	final String varLineRegex = "(int|double|String|char|boolean)(\\s+\\w+\\s+\\=\\w+\\,?)+";
 	final String methodStartRegex = "\\void\\s\\w";
 	final String startScopeRegex = "(if|while|void)\\(\\w+\\)";
 	final String endScopeRegex = "}";
+	static final String COMMENT_PREFIX = "\\\\";
 	
 	static final String LEGAL_CHARS = "\\!#\\$\\%\\&\\(\\)\\*\\+\\-\\.\\/\\:\\;\\<\\=\\>\\?@\\[\\]\\^\\_\\`{\\|}\\~";
 	BufferedReader buffer;
@@ -25,13 +26,24 @@ public class Parser {
 		setMethods();
 	}
 	
-	private Scope getChunk(Stack<String> parenthesesBalance) throws illegalValueException, noSuchTypeException{
+	private Scope getChunk(Stack<String> parenthesesBalance) throws illegalValueException, noSuchTypeException, IOException{
 		Scope sc = new Scope();
-		String currentLine = new String();
+		String currentLine = buffer.readLine();
 		while(!Pattern.matches(endScopeRegex, currentLine) && !parenthesesBalance.isEmpty()){
+			currentLine = currentLine.trim();
+			if (currentLine.startsWith(COMMENT_PREFIX)) {
+				currentLine = buffer.readLine();
+				continue;
+			}
 			if(Pattern.matches(varLineRegex, currentLine)){
 				Variable var = VariableFactory.createVariable(currentLine);
 				sc.addVar(var);
+			}
+			if(Pattern.matches(startScopeRegex, currentLine)){
+				//TO-DO
+			}
+			if(Pattern.matches(endScopeRegex, currentLine)){
+				//TO-DO
 			}
 		}
 		return sc;
@@ -45,3 +57,26 @@ public class Parser {
 		}
 	}
 }
+//
+////some int member
+//final int a = 5;
+//
+////Another int member
+//final int b = a;
+//
+////now - a string member
+//String s;
+//
+//void boo(int a, int b, String s) {
+//if (true) {
+//	boo(1,2,"hello");
+//	while (false) {
+//		soo(true);
+//	}
+//}
+//return;
+//}
+//
+//void soo(boolean b) {
+//return;
+//}
