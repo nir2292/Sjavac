342308115b647f793b477a9b8c2cb74f890413b4
nir2292 <nir.erez@gmail.com>
2015-06-13 15:00:14 +0300

    removed globalVariables<> instead: global vars are being fed into knownVariables<> updated addvar , addallvar

--- src/oop/ex6/scopes/Scope.java
+++ src/oop/ex6/scopes/Scope.java
@@ -4,28 +4,31 @@ import java.util.ArrayList;
 import oop.ex6.main.*;
 
 public class Scope {
-	private ArrayList<Variable> globalVariables;
+//	private ArrayList<Variable> globalVariables;
 	private ArrayList<Variable> knownVariables;
 	private ArrayList<Scope> internalScopes;
 	
 	public Scope(){
 		this.knownVariables = new ArrayList<>();
-		this.globalVariables = new ArrayList<>();
+//		this.globalVariables = new ArrayList<>();
 	}
 	
 	public Scope(ArrayList<Variable> vars){
 		this.knownVariables = new ArrayList<>();
-		this.globalVariables = new ArrayList<>();
+//		this.globalVariables = new ArrayList<>();
 		addAllVars(vars);
 	}
 	
 	public void addVar(Variable var){
 		knownVariables.add(var);
+		for (Scope internalScope : internalScopes) {
+			internalScope.addVar(var);
+		}
 	}
 	
 	public void addAllVars(ArrayList<Variable> vars){
 		for(Variable var:vars)
-			this.knownVariables.add(var);
+			addVar(var);
 	}
 	
 	public boolean knowsVariable(Variable var){
@@ -38,9 +41,9 @@ public class Scope {
 		for(Variable var:knownVariables)
 			if(var.getName().equals(varName))
 				return var;
-		for(Variable var:globalVariables)
-			if(var.getName().equals(varName))
-				return var;
+//		for(Variable var:globalVariables)
+//			if(var.getName().equals(varName))
+//				return var;
 		throw new noSuchVariable("Unknown variable" + varName);
 	}
 	
